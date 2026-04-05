59efc25ab099be05b3061a544d469f3d3cb44899
jeffe499 <144942612+jeffe499@users.noreply.github.com>
2026-04-05 13:53:54 -0300

    w

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
-// Permite ler dados do formulário
 app.use(express.urlencoded({ extended: true }));
 
-// Servir arquivos estáticos (CSS, áudio, etc.)
-app.use(express.static(__dirname));
-
-// 🔹 Página inicial
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// 🔹 Resultado
-app.post("/resultado", (req, res) => {
-    const nome = req.body.nome;
+app.get("/style.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "style.css"));
+});
 
+app.get("/musica.mp3", (req, res) => {
+    res.sendFile(path.join(__dirname, "musica.mp3"));
+});
+
+app.post("/resultado", (req, res) => {
+    const nome = req.body.nome || "Visitante";
     const porcentagem = Math.floor(Math.random() * 11) + 90;
 
     const frases = [
@@ -29,20 +31,15 @@ app.post("/resultado", (req, res) => {
     ];
 
     const motivo = frases[Math.floor(Math.random() * frases.length)];
-
     const resultado = `${nome}, seu nível de diversão é ${porcentagem}% - ${motivo}`;
 
-    // Lê HTML e injeta resultado
-    const fs = require("fs");
-    let html = fs.readFileSync("index.html", "utf-8");
-
+    let html = fs.readFileSync(path.join(__dirname, "index.html"), "utf-8");
     html = html.replace("{{RESULTADO}}", resultado);
     html = html.replace("{{MOSTRAR_RESULTADO}}", "true");
 
     res.send(html);
 });
 
-// Iniciar servidor
 app.listen(3000, () => {
     console.log("Servidor rodando em http://localhost:3000");
 });
